2b6939dcb5551bfce61e2b6d1313bbdee7ec8c84
Martin Beran <martin@mber.cz>
2020-05-02 16:53:54 +0200

    Bitwise operations on byte sequences and IPv4 addresses

--- netaddr_calc.sh
+++ netaddr_calc.sh
@@ -1,20 +1,172 @@
 # Include this file into a shell (/bin/sh) script by
 # . netaddr_calc.sh
 
+# In documentation comments of functions, P = parameters, O = stdout,
+# E = stderr, R = return value (exit status, 0 if unspecified)
+
+### Operations on IPv4 addresses #############################################
+
+# An IPv4 address is represented in the usual format of 4 decimal numbers
+# delimited by '.' characters.
+
+# ipv4_bits2mask BITS
+# Convert a number of bits to a bitmask
+# P: BITS = number of initial bits
+# O: an with initial BITS bits set to 1, remaining bits set to 0
+ipv4_bits2mask()
+{
+    bytes_bits2mask 4 $1
+}
+
+# ipv4_invert IP
+# Invert all bits of an IPv4 address
+# P: IP = IPv4 address
+# O: IP with all bits inverted
+ipv4_invert()
+{
+    bytes_invert $1
+}
+
+# ipv4_and IP1 IP2
+# Combine two IPv4 addresses by bitwise AND
+# P: IP1, IP2 = IPv4 addresses
+# O: addresses combined
+ipv4_and()
+{
+    bytes_and $1 $2
+}
+
+# ipv4_or IP1 IP2
+# Combine two IPv4 addresses by bitwise OR
+# P: IP1, IP2 = IPv4 addresses
+# O: addresses combined
+ipv4_or()
+{
+    bytes_or $1 $2
+}
+
+### Operations on IPv6 addresses #############################################
+
+### Operations on MAC (Ethernet) addresses ###################################
+
 ### IP/MASK pair #############################################################
 
+# ip_addrmask2addr ADDRMASK
 # Get the IP (v4/6) address from an "IP/MASK" pair.
-# P: address+mask
-# R: address
+# P: ADDRMASK = address/mask
+# O: address
 ip_addrmask2addr()
 {
     echo ${1%/*}
 }
 
+# ip_addrmask2mask ADDRMASK
 # Get the IP (v4/6) mask from an "IP/MASK" pair.
-# P: address+mask
-# R: mask
+# P: ADDRMASK = address/mask
+# O: mask
 ip_addrmask2mask()
 {
     echo ${1#*/}
+}
+
+### Operations on sequences of bytes #########################################
+
+# Auxiliary functions working with sequences of bytes, written as decimal
+# numbers delimited by '.'
+
+# bytes_bits2mask BYTES BITS
+# Convert a number of bits to a bitmask
+# P: BYTES = number of bytes in the result
+#    BITS = number of initial bits
+# O: a sequences of BYTES bytes with initial BITS bits set to 1, remaining bits
+#    set to 0
+bytes_bits2mask()
+{
+    local bytes bits i mask
+    bytes="$1"
+    bits="$2"
+    i=0
+    mask=''
+    while [ $i -lt $bytes ]; do
+        if [ $bits -ge 8 ]; then
+            mask="$mask.255"
+            bits=$((bits-8))
+        elif [ $bits = 0 ]; then
+            mask="$mask.0"
+        else
+            mask="$mask.$((255>>(8-bits)<<(8-bits)))"
+            bits=0
+        fi
+        i=$((i+1))
+    done
+    echo ${mask#.}
+}
+
+# Auxiliary functions for bytes_invert, bytes_and, bytes_or
+
+bytes_op_invert()
+{
+    echo $((255^$1))
+}
+
+bytes_op_and()
+{
+    echo $(($1&$2))
+}
+
+bytes_op_or()
+{
+    echo $(($1|$2))
+}
+
+bytes_apply()
+{
+    local op bytes1 bytes2 result b1 b2
+    op="$1"
+    bytes1="$2."
+    bytes2="$3."
+    result=""
+    while [ -n "$bytes1" ]; do
+        b1=${bytes1%%.*}
+        bytes1=${bytes1#*.}
+        b2=${bytes2%%.*}
+        bytes2=${bytes2#*.}
+        result="$result.`$op $b1 $b2`"
+    done
+    echo ${result#.}
+}
+
+# bytes_invert BYTES
+# Invert all bits of a sequence of bytes
+# P: BYTES = a sequence of bytes
+# O: BYTES with all bit values inverted
+bytes_invert()
+{
+    local bytes
+    bytes="$1"
+    bytes_apply bytes_op_invert $bytes $bytes
+}
+
+# bytes_and BYTES1 BYTES2
+# Apply bitwise AND to corresponding bits of two sequences of bytes
+# P: BYTES1, BYTES2 = input sequences of the same number of bytes
+# O: result of bitwise AND
+bytes_and()
+{
+    local bytes1 bytes2
+    bytes1="$1"
+    bytes2="$2"
+    bytes_apply bytes_op_and $bytes1 $bytes2
+}
+
+# bytes_or BYTES1 BYTES2
+# Apply bitwise OR to corresponding bits of two sequences of bytes
+# P: BYTES1, BYTES2 = input sequences of the same number of bytes
+# O: result of bitwise OR
+bytes_or()
+{
+    local bytes1 bytes2
+    bytes1="$1"
+    bytes2="$2"
+    bytes_apply bytes_op_or $bytes1 $bytes2
 }
--- netaddr_calc_test.sh
+++ netaddr_calc_test.sh
@@ -56,14 +56,6 @@ test()
 # Sets of tests, each is a function named test_* and its name (without test_)
 # must be included in variable TEST_SUITES.
 
-# Test the test environment
-test_test()
-{
-    test true true 0 ''
-    test false false 1 ''
-    test echo echo 0 'hello world' hello world
-}
-
 # Test address+mask splitting
 test_addrmask()
 {
@@ -74,8 +66,94 @@ test_addrmask()
     test mask6 ip_addrmask2mask 0 64 2001:470:5a02:0:9441:4471:82d3:368c/64
 }
 
+# Test operations with byte sequences
+test_bytes()
+{
+    test bits2mask1_0 bytes_bits2mask 0 0 1 0
+    test bits2mask1_1 bytes_bits2mask 0 128 1 1
+    test bits2mask1_2 bytes_bits2mask 0 192 1 2
+    test bits2mask1_3 bytes_bits2mask 0 224 1 3
+    test bits2mask1_4 bytes_bits2mask 0 240 1 4
+    test bits2mask1_5 bytes_bits2mask 0 248 1 5
+    test bits2mask1_6 bytes_bits2mask 0 252 1 6
+    test bits2mask1_7 bytes_bits2mask 0 254 1 7
+    test bits2mask1_8 bytes_bits2mask 0 255 1 8
+
+    test bitmask2mask4_0 bytes_bits2mask 0 0.0.0.0 4 0
+    test bitmask2mask4_1 bytes_bits2mask 0 128.0.0.0 4 1
+    test bitmask2mask4_8 bytes_bits2mask 0 255.0.0.0 4 8
+    test bitmask2mask4_10 bytes_bits2mask 0 255.192.0.0 4 10
+    test bitmask2mask4_16 bytes_bits2mask 0 255.255.0.0 4 16
+    test bitmask2mask4_20 bytes_bits2mask 0 255.255.240.0 4 20
+    test bitmask2mask4_24 bytes_bits2mask 0 255.255.255.0 4 24
+    test bitmask2mask4_25 bytes_bits2mask 0 255.255.255.128 4 25
+    test bitmask2mask4_30 bytes_bits2mask 0 255.255.255.252 4 30
+    test bitmask2mask4_31 bytes_bits2mask 0 255.255.255.254 4 31
+    test bitmask2mask4_32 bytes_bits2mask 0 255.255.255.255 4 32
+
+    test bitmask2mask16_0 bytes_bits2mask 0 \
+        0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0 16 0
+    test bitmask2mask16_64 bytes_bits2mask 0 \
+        255.255.255.255.255.255.255.255.0.0.0.0.0.0.0.0 16 64
+
+    test bytes_invert_1 bytes_invert 0 0 255
+    test bytes_invert_2 bytes_invert 0 255 0
+    test bytes_invert_3 bytes_invert 0 1 254
+    test bytes_invert_4 bytes_invert 0 254 1
+    test bytes_invert_5 bytes_invert 0 129 126
+    test bytes_invert_6 bytes_invert 0 126 129
+    test bytes_invert_7 bytes_invert 0 255.254.127.15.170.0 0.1.128.240.85.255
+    test bytes_invert_8 bytes_invert 0 0.1.128.240.85.255 255.254.127.15.170.0
+
+    test bytes_and_1 bytes_and 0 0 0 0
+    test bytes_and_2 bytes_and 0 0 0 1
+    test bytes_and_3 bytes_and 0 0 1 0
+    test bytes_and_4 bytes_and 0 1 1 1
+    test bytes_and_5 bytes_and 0 0.0.0.14 0.255.0.254 0.0.15.15
+
+    test bytes_or_1 bytes_or 0 0 0 0
+    test bytes_or_2 bytes_or 0 1 0 1
+    test bytes_or_3 bytes_or 0 1 1 0
+    test bytes_or_4 bytes_or 0 1 1 1
+    test bytes_and_5 bytes_or 0 0.255.15.255 0.255.0.254 0.0.15.15
+}
+
+# Test operations with IPv4 addresses
+test_ipv4()
+{
+    test bits2mask_0 ipv4_bits2mask 0 0.0.0.0 0
+    test bits2mask_8 ipv4_bits2mask 0 255.0.0.0 8
+    test bits2mask_12 ipv4_bits2mask 0 255.240.0.0 12
+    test bits2mask_16 ipv4_bits2mask 0 255.255.0.0 16
+    test bits2mask_24 ipv4_bits2mask 0 255.255.255.0 24
+
+    test ipv4_invert_1 ipv4_invert 0 255.255.255.0 0.0.0.255
+    test ipv4_invert_2 ipv4_invert 0 0.0.0.255 255.255.255.0
+    test ipv4_invert_3 ipv4_invert 0 255.240.0.0 0.15.255.255
+    test ipv4_invert_4 ipv4_invert 0 0.15.255.255 255.240.0.0
+
+    test ipv4_and_1 ipv4_and 0 192.168.2.0 192.168.2.1 255.255.255.0
+    test ipv4_and_2 ipv4_and 0 172.16.0.0 172.20.2.1 255.240.0.0
+    test ipv4_or_1 ipv4_or 0 172.20.2.1 172.16.0.0 0.4.2.1
+    test ipv4_or_2 ipv4_or 0 192.168.31.255 192.168.16.1 0.0.15.255
+}
+
+# Test the test environment
+test_test()
+{
+    test true true 0 ''
+    test false false 1 ''
+    test echo echo 0 'hello world' hello world
+}
+
+# Temporary tests
+test_tmp()
+{
+    :
+}
+
 # List of test suites (separated by whitespace)
-TEST_SUITES='addrmask test'
+TEST_SUITES='addrmask bytes ipv4 test tmp'
 
 ### Entry point ##############################################################
 
